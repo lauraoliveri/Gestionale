a16ed9182140e79e2759fb0534cfb3a901efb490
lauraoliveri <lauraoliveri2003@gmail.com>
2025-10-22 22:11:13 +0200

    Add new class 'Storico' and 'Ruolo' in 'model' package

---
+++ src/main/java/model/Ruolo.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Ruolo {
+
+    private int idRuolo;
+    private String descrizione;
+
+    public int getIdRuolo() {
+        return idRuolo;
+    }
+
+    public void setIdRuolo(int idRuolo) {
+        this.idRuolo = idRuolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
---
+++ src/main/java/model/Storico.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Storico {
+
+    private int idStorico;
+    private int matricola;
+    private int idRuolo;
+    private LocalDate dataInizio;
+    private LocalDate DataFine;
+
+    public int getIdStorico() {
+        return idStorico;
+    }
+
+    public void setIdStorico(int idStorico) {
+        this.idStorico = idStorico;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(int matricola) {
+        this.matricola = matricola;
+    }
+
+    public int getIdRuolo() {
+        return idRuolo;
+    }
+
+    public void setIdRuolo(int idRuolo) {
+        this.idRuolo = idRuolo;
+    }
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDate getDataFine() {
+        return DataFine;
+    }
+
+    public void setDataFine(LocalDate dataFine) {
+        DataFine = dataFine;
+    }
+}
